01fa3e873adb6a217eb0e12d2d0fbedb838df6b2
lixuanqi <lixuanqi1995@gmail.com>
2018-05-29 17:58:06 +0800

    UI

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,9 +1,29 @@
 
 Page({
   data: {
-    logs: []
+    tabList: [{
+      id: '1',
+      title: '全部评论'
+    }, {
+      id: '2',
+      title: '已回复'
+    }],
+    selectedTabId: '1',
   },
   onLoad: function () {
-   
-  }
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#F9D173',
+      animation: {
+        duration: 400,
+        timingFunc: 'easeIn'
+      }
+    })
+  },
+  handleTabChanged: function (e) {
+    const id = e.detail;
+    this.setData({
+      selectedTabId: id
+    });
+  },
 })
