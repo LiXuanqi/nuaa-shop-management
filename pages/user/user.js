fbba8c9a0511711e606589653cbc087385ef7797
lixuanqi <lixuanqi1995@gmail.com>
2018-07-29 22:18:02 +0800

    finish user page.

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,3 +1,6 @@
+import moment from '../../utils/moment';
+
+const app = getApp();
 
 Page({
   data: {
@@ -9,6 +12,8 @@ Page({
       title: '已回复'
     }],
     selectedTabId: '1',
+    comments: [],
+    replyComments: []
   },
   onLoad: function () {
     wx.setNavigationBarColor({
@@ -19,6 +24,7 @@ Page({
         timingFunc: 'easeIn'
       }
     })
+    this.getUserComments();
   },
   handleTabChanged: function (e) {
     const id = e.detail;
@@ -26,4 +32,37 @@ Page({
       selectedTabId: id
     });
   },
+  getUserComments: function () {
+    wx.request({
+      url: 'https://nuaashop.yuwenjie.cc/?service=App.Shop.GetUserComment',
+      method: 'POST',
+      data: {
+        session: app.globalData.code
+      },
+      success: ({ data }) => {
+        let formatedData = data['data'].map((item) => {
+          return {
+            ...item,
+            date: item.date.split(" ")[0],
+            adminReplyStatus: moment(parseInt(item.adminReplyStatus)).startOf('day').fromNow().split(" ")[0], // calculate interval of days.
+            ownerReplyStatus: moment(parseInt(item.ownerReplyStatus)).startOf('day').fromNow().split(" ")[0],
+          }
+        }) 
+        console.log(formatedData)
+        this.setData({
+          comments: formatedData
+        })
+        const replyComments = replyComments.filter((item) => {
+          if (item.adminReply || item.ownerReply) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+        this.setData({
+          replyComments: replyComments
+        })
+      }
+    })
+  }
 })
